af18ebc636738b47ef8af01ce08d43b95853f9bf
HaoChenZhu <xiaohao99.ch@gmail.com>
2023-05-02 20:04:33 +0200

    Deploy Heroku 7

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ const path = require("path");
 const app = express();
 
 // Sirve solo los archivos estáticos de la carpeta dist
-app.use(express.static(__dirname + "/dist/qrnotify"));
+app.use(express.static("./dist/qrnotify"));
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/qrnotify/index.html"));
-});
-
+app.get("/*", (req, res) =>
+  res.sendFile("index.html", { root: "dist/qrnotify/" })
+);
 // Inicia la aplicación escuchando en el puerto de Heroku o en el puerto 8080
 app.listen(process.env.PORT || 8080);
